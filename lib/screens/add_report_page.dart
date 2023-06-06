38e5a894159607ac0a84ffe17ee2424d8fada49b
Lans12345 <olanalans12345@gmail.com>
2023-06-06 13:55:48 +0800

    added report page

--- lib/screens/add_report_page.dart
+++ lib/screens/add_report_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sunspark/widgets/button_widget.dart';
 import 'package:sunspark/widgets/text_widget.dart';
 import 'package:sunspark/widgets/textfield_widget.dart';
+import 'package:intl/intl.dart';
 
 class AddReportPage extends StatefulWidget {
   const AddReportPage({super.key});
@@ -32,9 +34,12 @@ class _AddReportPageState extends State<AddReportPage> {
   ];
 
   String selected = 'Theft';
+  var selectedDateTime = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
+    final formattedDateTime =
+        DateFormat('yyyy-MM-dd hh:mm a').format(selectedDateTime);
     return Scaffold(
         appBar: AppBar(
           title: TextRegular(
@@ -43,50 +48,53 @@ class _AddReportPageState extends State<AddReportPage> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextBold(
-                text: 'Witness Information',
-                fontSize: 18,
-                color: Colors.black,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFieldWidget(
-                  width: 350, label: 'Name', controller: nameController),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFieldWidget(
-                  width: 350,
-                  label: 'Phone Number',
-                  controller: numberController),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFieldWidget(
-                  width: 350, label: 'Address', controller: addressController),
-              const SizedBox(
-                height: 20,
-              ),
-              TextBold(
-                text: 'Incident Information',
-                fontSize: 18,
-                color: Colors.black,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10, bottom: 10),
-                child: TextRegular(
-                    text: 'Incident Type:', fontSize: 14, color: Colors.black),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10, right: 10),
-                child: Container(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextBold(
+                  text: 'Witness Information',
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldWidget(
+                    width: 350, label: 'Name', controller: nameController),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldWidget(
+                    width: 350,
+                    label: 'Phone Number',
+                    controller: numberController),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldWidget(
+                    width: 350,
+                    label: 'Address',
+                    controller: addressController),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextBold(
+                  text: 'Incident Information',
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10, bottom: 10),
+                  child: TextRegular(
+                      text: 'Incident Type:',
+                      fontSize: 14,
+                      color: Colors.black),
+                ),
+                Container(
                   decoration: BoxDecoration(
                       border: Border.all(
                         color: Colors.black,
@@ -122,9 +130,203 @@ class _AddReportPageState extends State<AddReportPage> {
                     },
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  onTap: () {
+                    _selectDateTime(context);
+                  },
+                  child: IgnorePointer(
+                    child: TextFormField(
+                      controller: TextEditingController(
+                        text: formattedDateTime,
+                      ),
+                      decoration: const InputDecoration(
+                        labelText: 'Date and Time',
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  color: Colors.black,
+                  height: 200,
+                  width: double.infinity,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldWidget(
+                    width: 350,
+                    height: 50,
+                    label: 'Statement',
+                    controller: statementController),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextBold(
+                  text: 'Evidence (photo)',
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  color: Colors.black,
+                  height: 100,
+                  width: double.infinity,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.file_upload,
+                        color: Colors.black,
+                      ),
+                      label: TextRegular(
+                          text: 'Browse Gallery',
+                          fontSize: 12,
+                          color: Colors.grey),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.camera,
+                        color: Colors.black,
+                      ),
+                      label: TextRegular(
+                          text: 'Camera', fontSize: 12, color: Colors.grey),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: false,
+                      onChanged: (value) {
+                        setState(() {});
+                      },
+                    ),
+                    const SizedBox(
+                      width: 300,
+                      child: Text(
+                        'I certify that the information I provided in this form is accurate and true.',
+                        style: TextStyle(fontFamily: 'QRegular'),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: false,
+                      onChanged: (value) {
+                        setState(() {});
+                      },
+                    ),
+                    const SizedBox(
+                      width: 300,
+                      child: Text(
+                        'I understand that any false statements I provided can be used against me.',
+                        style: TextStyle(fontFamily: 'QRegular'),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: false,
+                      onChanged: (value) {
+                        setState(() {});
+                      },
+                    ),
+                    const SizedBox(
+                      width: 300,
+                      child: Text(
+                        'I understand that this document will be considered strictly condifential.',
+                        style: TextStyle(fontFamily: 'QRegular'),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Center(
+                    child: ButtonWidget(
+                        label: 'Add Report',
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: TextBold(
+                                    text: 'Alert',
+                                    fontSize: 18,
+                                    color: Colors.black),
+                                content: TextRegular(
+                                    text:
+                                        'Your report was succesfully submitted!',
+                                    fontSize: 14,
+                                    color: Colors.grey),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: TextRegular(
+                                        text: 'Close',
+                                        fontSize: 14,
+                                        color: Colors.black),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        })),
+                const SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
           ),
         ));
+  }
+
+  Future<void> _selectDateTime(BuildContext context) async {
+    final DateTime? pickedDateTime = await showDatePicker(
+      context: context,
+      initialDate: selectedDateTime,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.fromDateTime(selectedDateTime),
+    );
+
+    setState(() {
+      selectedDateTime = DateTime(
+        pickedDateTime!.year,
+        pickedDateTime.month,
+        pickedDateTime.day,
+        pickedTime!.hour,
+        pickedTime.minute,
+      );
+    });
   }
 }
