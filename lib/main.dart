c36e15d0741d9963b735978ddef2d40abd066cd9
Lans12345 <olanalans12345@gmail.com>
2023-07-06 10:59:44 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sunspark/screens/auth/landing_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'sunspark-efc9c',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
