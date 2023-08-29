29c1799b5d592c6a31110219d9c63279eedcc70a
Lans12345 <olanalans12345@gmail.com>
2023-08-29 14:04:15 +0800

    added pin location func

--- lib/screens/add_report_page.dart
+++ lib/screens/add_report_page.dart
@@ -29,6 +29,7 @@ class _AddReportPageState extends State<AddReportPage> {
   @override
   void initState() {
     super.initState();
+    determinePosition();
     addMarker();
   }
 
@@ -83,19 +84,26 @@ class _AddReportPageState extends State<AddReportPage> {
       setState(() {
         lat = position.latitude;
         long = position.longitude;
+
+        markers.add(
+          Marker(
+            draggable: true,
+            onDrag: (value) {
+              setState(() {
+                lat = value.latitude;
+                long = value.longitude;
+              });
+            },
+            icon: BitmapDescriptor.defaultMarker,
+            markerId: const MarkerId('my location'),
+            position: LatLng(position.latitude, position.longitude),
+          ),
+        );
         hasLoaded = true;
       });
     }).catchError((error) {
       print('Error getting location: $error');
     });
-
-    markers.add(
-      Marker(
-        icon: BitmapDescriptor.defaultMarker,
-        markerId: const MarkerId('my location'),
-        position: LatLng(lat, long),
-      ),
-    );
   }
 
   Future<void> uploadPicture(String inputSource) async {
@@ -364,6 +372,7 @@ class _AddReportPageState extends State<AddReportPage> {
                               height: 200,
                               width: double.infinity,
                               child: GoogleMap(
+                                markers: markers,
                                 myLocationEnabled: true,
                                 myLocationButtonEnabled: true,
                                 onCameraMove: (position) {
@@ -373,8 +382,8 @@ class _AddReportPageState extends State<AddReportPage> {
                                   });
                                 },
                                 mapType: MapType.normal,
-                                initialCameraPosition: const CameraPosition(
-                                  target: LatLng(10.2477, 122.9888),
+                                initialCameraPosition: CameraPosition(
+                                  target: LatLng(lat, long),
                                   zoom: 14.4746,
                                 ),
                                 onMapCreated: (GoogleMapController controller) {
@@ -615,5 +624,42 @@ class _AddReportPageState extends State<AddReportPage> {
         pickedTime.minute,
       );
     });
+  }
+
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 }
